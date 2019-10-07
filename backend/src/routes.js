c69579f018a4cc95debe3cb7877922a858fe2250
Breno lopes de souza <breno_lopes_10@yahoo.com.br>
2019-10-07 08:43:28 -0300

    iniciando projeto, criando rotas, controllers e configurando multer.

---
+++ backend/src/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const routes = express.Router();
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+
+routes.post('/sessions',SessionController.store)
+routes.post('/spots',SpotController.store)
+module.exports = routes;
